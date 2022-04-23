9e3d48861aba6a5054b0a989dee718fb872d01b5
karis1234 <qkzkf3487@naver.com>
2022-04-23 15:52:05 +0900

    리포지터리,데이터 조회하기 findBySubjectAndContent

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Optional<Question> findBySubject(String subject);
+    Question findBySubject(String subject);
+
+    Question findBySubjectAndContent(String subject, String content);
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -57,7 +57,13 @@ class SbbApplicationTests {
 
 	@Test
 	void testFindBySubject(){
-		Question q = questionRepository.findBySubject("sbb가 무엇인가요?").get();
+		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(q.getId(),1);
+	}
+
+	@Test
+	void testFindBySubjectAndContent(){
+		Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?","sbb에 대해 알고 싶습니다.");
 		assertEquals(q.getId(),1);
 	}
 
