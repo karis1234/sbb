2a05798641c70829c4810a7391b7192b0e4554d9
karis1234 <qkzkf3487@naver.com>
2022-04-23 15:43:43 +0900

    리포지터리,데이터 조회하기 findById

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -35,13 +36,23 @@ class SbbApplicationTests {
 	}
 
 	@Test
-	void testJpa() {
+	void testFindAll() {
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(2,all.size());
 
 		Question q = all.get(0);
 		assertEquals("sbb가 무엇인가요?",q.getSubject());
 
+	}
+
+	@Test
+	void testFindById(){
+		Optional<Question> oq = questionRepository.findById(1);
+
+		if(oq.isPresent()){
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 
 }
