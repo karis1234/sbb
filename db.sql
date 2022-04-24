43e4105ba73a70a67e543329078746c03f2592ba
karis1234 <qkzkf3487@naver.com>
2022-04-24 15:30:13 +0900

    db.sql 테스트용 질문 2개 생성

--- db.sql
+++ db.sql
@@ -15,3 +15,13 @@ CREATE TABLE question (
     `subject` VARCHAR(200) NOT NULL,
     content TEXT NOT NULL
 );
+
+INSERT INTO question
+SET create_date = NOW(),
+`subject` = 'sbb가 무엇인가요?',
+content = 'sbb에 대해서 알고 싶습니다.';
+
+INSERT INTO question
+SET create_date = NOW(),
+`subject` = '스프링부트 모델 질문입니다.',
+content = 'id는 자동으로 생성되나요?';
