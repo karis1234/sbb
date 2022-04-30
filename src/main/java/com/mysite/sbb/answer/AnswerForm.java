d80e9333c504ad517c2d4c3e0c1dc02339a24f26
karis1234 <qkzkf3487@naver.com>
2022-04-30 15:53:40 +0900

    오류 메세지 공통 템플릿

--- src/main/java/com/mysite/sbb/answer/AnswerForm.java
+++ src/main/java/com/mysite/sbb/answer/AnswerForm.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
